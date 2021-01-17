b9a800f38d364cbe24bb81ad50945f0df1f2f813
Bogdan Damian <bogdann.damian@gmail.com>
2021-01-17 09:22:03 +0100

    added monster ds

--- src/App.js
+++ src/App.js
@@ -5,18 +5,31 @@ import './App.css';
 class App extends Component {
     constructor(props) {
         super(props);
-        this.state = {displayMsg: 'Mainframes on the web...'}
+        this.state = {
+            monsters: [
+                {
+                    id: '8e385a63-de1e-4535-af8c-598f6a27a22a',
+                    name: 'Frankenstein'
+                },
+                {
+                    id: 'b8547809-d51c-4491-a38b-1da0f1c75f3e',
+                    name: 'Dracula'
+                },
+                {
+                    id: '0e332c6a-6cc9-4fc4-905c-f18a2bcd83fc',
+                    name: 'Zombie'
+                }
+            ]
+        }
+
     }
 
     render() {
         return (
             <div className="App">
-                <header className="App-header">
-                    <img src={logo} className="App-logo" alt="logo"/>
-                    <p>{this.state.displayMsg}
-                    </p>
-                    <button onClick={() => this.setState({displayMsg: 'yes they are'})}>Click me!</button>
-                </header>
+                {
+                    this.state.monsters.map(monster => <h1 key={monster.id}>{monster.name}</h1>)
+                }
             </div>
         );
     }
