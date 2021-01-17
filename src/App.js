f97f0808ce24484124fd2dd8f0cdb22e17e06116
Bogdan Damian <bogdann.damian@gmail.com>
2021-01-17 08:31:41 +0100

    first encounter

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Va fi bomba.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {displayMsg: 'Mainframes on the web...'}
+    }
+
+    render() {
+        return (
+            <div className="App">
+                <header className="App-header">
+                    <img src={logo} className="App-logo" alt="logo"/>
+                    <p>{this.state.displayMsg}
+                    </p>
+                    <button onClick={() => this.setState({displayMsg: 'yes they are'})}>Click me!</button>
+                </header>
+            </div>
+        );
+    }
 }
 
 export default App;
